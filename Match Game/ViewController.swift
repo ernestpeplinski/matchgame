eb0d5cbb9f17788818a5a320ba68b5b81fcd6a1c
ernestpeplinski <ernestpeplinski@gmail.com>
2020-05-23 13:15:25 -0700

    Add Game Logic

--- Match Game/ViewController.swift
+++ Match Game/ViewController.swift
@@ -16,6 +16,11 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     var model = CardModule()
     var cardArray = [Card]()
     
+        
+    var firstFlippedCardIndex : IndexPath?
+    var timer : Timer?
+    var milliseconds: Float = 30 * 1000 // 30 sec
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.delegate = self
@@ -37,32 +42,107 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
        
        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
            
-           
-           // get a cardCollectionViewCell object
+
            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath) as! CardCollectionViewCell
            
-           //Get the card that the colletion view is trying to display
+
            let card = cardArray[indexPath.row]
            
-           
-           //set that card for the cell
+
            cell.setCard(card)
            
                    return cell
            
        }
        
-       func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-         let cell =  collectionView.cellForItem(at: indexPath) as! CardCollectionViewCell
-        let card = cardArray[indexPath.row]
-        if card.isFliped == false {
-            cell.flip()
-            card.isFliped = true
-         }else{
-            cell.flipBack()
-            card.isFliped = false
-            
-        }
-        } //Engding of didSelectItem method.
+        func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+             
+             if milliseconds <= 0 {
+                 return
+             }
+             
+            let cell =  collectionView.cellForItem(at: indexPath) as! CardCollectionViewCell
+             
+             let card = cardArray[indexPath.row]
+             
+             if card.isFliped == false && card.isMatched == false {
+
+                 cell.flip()
+                 card.isFliped = true
+                 if firstFlippedCardIndex == nil {
+
+                     firstFlippedCardIndex = indexPath
+                 } else {
+                     
+                     checkForMatches(indexPath)
+                 }
+             }
+             
+             
+         } //Engding of didSelectItem method.
+         
+    
+         func checkForMatches(_ secondFlippedCardIndex: IndexPath){
+
+             let cardOneCell = collectionView.cellForItem(at: firstFlippedCardIndex!) as? CardCollectionViewCell
+             
+             let cardTwoCell = collectionView.cellForItem(at: secondFlippedCardIndex) as? CardCollectionViewCell
+             
+
+             let cardOne = cardArray[firstFlippedCardIndex!.row]
+             let cardTwo = cardArray[secondFlippedCardIndex.row]
+
+            if cardOne.imageName == cardTwo.imageName {
+           
+             cardOne.isMatched = true
+             cardTwo.isMatched = true
+             
+             cardOneCell?.remove()
+             cardTwoCell?.remove()
+             
+             ckeckGameEnded()
+             
+             }
+             else {
+             cardOne.isFliped = false
+             cardTwo.isFliped = false
+                 
+             cardOneCell?.flipBack()
+             cardTwoCell?.flipBack()
+                 
+             }
+
+             if cardOneCell == nil {
+                 collectionView.reloadItems(at: [firstFlippedCardIndex!])
+             }
+
+             firstFlippedCardIndex = nil
+         }
+         func ckeckGameEnded() {
+             
+             var isWon = true
+             
+             for card in cardArray {
+                 
+                 if card.isMatched == false {
+                     isWon = false
+                     break
+                 }
+             }
+
+             if isWon == true {
+                 
+                 if milliseconds > 0{
+                     timer?.invalidate()
+                 }
+
+                 
+             } else {
+                 if milliseconds > 0 {
+                     return
+                 }
+             }
+         }
+
 }
 
